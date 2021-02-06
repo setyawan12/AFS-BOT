a580fac8508656ad4a0748ba73f79a01a126acc6
unknown <bocahgendut005@gmail.com>
2021-02-06 08:00:12 +0700

    pembaruan

--- src/help.js
+++ src/help.js
@@ -1,37 +1,30 @@
 const help = (prefix) => {
-	return `> *Sticker Commands* <
-command : *${prefix}sticker* or *${prefix}stiker*
-desc : convert image/gif/video to sticker
-usage : reply image/gif/video, or send image/gif/video with caption\n
-command : *${prefix}sticker nobg* or *${prefix}stiker nobg*
-desc : convert image to sticker while removing the background
-usage : reply image, or send image with caption\n
-command : *${prefix}toimg*
-desc : convert sticker to image
-usage : reply sticker\n
-> *Others Commands* <
-command : *${prefix}tts*
-desc : convert text to speech/audio
-usage : *${prefix}tts [cc] [text]*\nexample : *${prefix}tts ja On2-chan*\n
-command : *${prefix}ocr*
-desc : take the text in the picture
-usage : reply image, or send image with caption\n
-command : *${prefix}wait*
-desc : search anime with image [ What Anime Is This/That ]
-usage : reply image, or send image with caption\n
-command : *${prefix}setprefix*
-desc : replace prefix
-usage : *${prefix}setprefix [text|optional]*\nexample : *${prefix}setprefix ?*
-note : This command can only be used by the bot owner\n
+	return `AFS BOT
+*Sticker Commands* 
+1.*${prefix}sticker*
+2.*${prefix}toimg*
+
+*Others Commands*
+1.*${prefix}tts*
+2.*${prefix}ocr*
+3.*${prefix}wait*
+4.*${prefix}translate |bahasa1|bahasa2|teks*
+5.*${prefix}cekzodiak <nama tanggal-bulan-tahun>*
+6.*${prefix}cekjodoh <namamu pasanganmu>*
+7.*${prefix}happymod <query>*
+
+
 > *Group Comands* <
-command : *${prefix}linkgroup*
-desc : take the group link
-usage : just send the command\
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}tagall*
-desc : tags all group members including admins too
-usage : just send the command
-note : This command can be used if you are a group admin\n`
+1.*${prefix}linkgroup*
+2.*${prefix}tagall*
+3. *${prefix}kick*
+4. *${prefix}add*
+
+*author*
+1.*${prefix}setprefix*
+2.*${prefix}bc*
+3.*${prefix}clearall*
+`
 }
 
 exports.help = help
